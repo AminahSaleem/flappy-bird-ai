8362661b8260a474e5befc74d2fa54f402331985
Aminah <aminahpsaleem@gmail.com>
2023-11-10 16:16:33 +0000

    sets the width and the height aswell as increases the image size and joins them

--- flappy_bird_tutorial.py
+++ flappy_bird_tutorial.py
@@ -3,3 +3,8 @@ import neat
 import time 
 import os 
 import random 
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
