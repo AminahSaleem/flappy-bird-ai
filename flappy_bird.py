6175a6a5794e95bac25a9b2394c88d706681b5fb
Aminah <aminahpsaleem@gmail.com>
2023-11-10 17:09:27 +0000

    sets the positioning of the bird

--- flappy_bird.py
+++ flappy_bird.py
@@ -17,3 +17,13 @@ class Bird:
     MAX_ROTATION = 25
     ROT_VEL = 20
     ANIMATION_TIME = 5  
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tilt_count = 0
+        self.vel = 0
+        self.height = self.y 
+        self.img_count = 0 
+        self.img = self.IMGS[0]
