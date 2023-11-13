fb201d04b9d39f4fb1e7862f63ce6fdc76dea1e2
Aminah <aminahpsaleem@gmail.com>
2023-11-13 00:54:01 +0000

    sets logic for bird collision

--- flappy_bird.py
+++ flappy_bird.py
@@ -75,7 +75,10 @@ class Bird:
             
         rotated_image = pygame.trandform.rotate(self.img, self.tilt)
         new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
-        win.blit(rotated_image, new_rect.topleft)                   
+        win.blit(rotated_image, new_rect.topleft)  
+        
+    def get_mask(self):         
+        return pygame.mask.from_surface(self.img)            
 while True:
     bird.move()        
         
