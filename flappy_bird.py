bdd679076f096fbdc1bc46091bc0ad98c4354b1c
Aminah <aminahpsaleem@gmail.com>
2023-11-14 01:00:46 +0000

    function to draw the base out

--- flappy_bird.py
+++ flappy_bird.py
@@ -148,7 +148,11 @@ class  Base:
              self.x1 = self.x2 + self.WIDTH
              
          if self.x2 + self.WIDTH < 0:
-             self.x2 = self.x1 + self.WIDTH  
+             self.x2 = self.x1 + self.WIDTH
+             
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y)) 
+        win.blit(self.IMG, (self.x2, self.y))    
         
                    
         
