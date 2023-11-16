ee700ac44672cf20c895017c8c8ff1bcbff6f233
Aminah <aminahpsaleem@gmail.com>
2023-11-16 18:06:54 +0000

    sets the game for multiple birds

--- flappy_bird.py
+++ flappy_bird.py
@@ -174,8 +174,9 @@ def draw_window(win, bird, pipes, base, score):
     pygame.display.update()
 
 
-def main():
-    bird = Bird(230, 350)
+def main(genomes, config):
+    birds = []
+    
     base = Base(730)
     pipes = [Pipe(700)]
     score = 0
@@ -194,8 +195,9 @@ def main():
         add_pipe = False 
         rem = []       
         for pipe in pipes:
-            if pipe.collide(bird):
-                pass
+            for bird in birds:
+                if pipe.collide(bird):
+                    pass
             
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
                  rem.append(pipe) 
@@ -212,9 +214,10 @@ def main():
                 
         for r in rem:
             pipes.remove(r)
-                    
-        if bird.y + bird.img.get_height() >= 730:
-            pass           
+            
+        for bird in birds:            
+            if bird.y + bird.img.get_height() >= 730:
+                pass           
 
         base.move()
         draw_window(win, bird, pipes, base, score)
@@ -225,11 +228,17 @@ def main():
 
 main()
 
-def run():
+def run(config_path):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
                                 neat.DefaultSpeciesSet, neat.DefaultStagnation,
                                 config_path)
-    pass
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    
+    winner = p.run()
+
 
 if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
