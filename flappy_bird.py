3953c31164e64bd6c1b240ab23b6a47fd9944029
Aminah <aminahpsaleem@gmail.com>
2023-11-22 14:17:35 +0000

    renders the current generation manager

--- flappy_bird.py
+++ flappy_bird.py
@@ -159,14 +159,17 @@ class  Base:
         
                    
         
-def draw_window(win, bird, pipes, base, score):
+def draw_window(win, bird, pipes, base, score, gen):
     win.blit(BG_IMG, (0, 0))
     
     for pipe in pipes:
         pipe.draw(win)
         
     text = STAT_FONT.render("Score: " + str(score), 1,(255,255,255))
-    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))    
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))   
+    
+    text = STAT_FONT.render("Gen: " + str(gen), 1,(255,255,255))
+    win.blit(text, (10, 10))     
         
     base.draw(win)  
       
