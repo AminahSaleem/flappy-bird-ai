5b4fa9477faf2997eb99b44666601a700d89e67c
Aminah <aminahpsaleem@gmail.com>
2023-11-22 14:29:19 +0000

    errpr handling

--- flappy_bird.py
+++ flappy_bird.py
@@ -161,7 +161,7 @@ class  Base:
         
                    
         
-def draw_window(win, bird, pipes, base, score, gen):
+def draw_window(win, birds, pipes, base, score, gen):
     win.blit(BG_IMG, (0, 0))
     
     for pipe in pipes:
@@ -183,23 +183,22 @@ def draw_window(win, bird, pipes, base, score, gen):
 
 def main(genomes, config):
     global GEN
-    GEN +=1 
+    GEN += 1
     nets = []
     ge = []
     birds = []
-    
-    
+
     for _, g in genomes:
-        net = net.nn.FeedForwardNetwork.create(g, config)
+        net = neat.nn.FeedForwardNetwork.create(g, config)
         nets.append(net)
         birds.append(Bird(230, 350))
-        g.fitness = 0 
+        g.fitness = 0
         ge.append(g)
-    
+
     base = Base(730)
     pipes = [Pipe(700)]
     score = 0
-    
+
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
 
@@ -207,69 +206,73 @@ def main(genomes, config):
     while run:
         clock.tick(30)
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
                 quit()
 
-                
         pipe_ind = 0
         if len(birds) > 0:
             if len(pipes) > 1 and birds[0].x + pipes[0].x + pipes[0].PIPE_TOP.get_width():
                 pipe_ind = 1
         else:
             run = False
-            break            
-                
+            break
+
+        birds_to_remove = []
         for x, bird in enumerate(birds):
             bird.move()
             ge[x].fitness += 0.1
-            
-            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))  
-            
+
+            output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
+
             if output[0] > 0.5:
-                bird.jump()         
-        
-        add_pipe = False 
-        rem = []       
+                bird.jump()
+
+            if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
+                birds_to_remove.append(x)
+
+        for x in birds_to_remove:
+            birds.pop(x)
+            nets.pop(x)
+            ge.pop(x)
+
+        add_pipe = False
+        rem = []
         for pipe in pipes:
             for x, bird in enumerate(birds):
                 if pipe.collide(bird):
                     ge[x].fitness -= 1
-                    birds.pop(x)
-                    nets.pop(x)
-                    ge.pop(x)
-            
+                    birds_to_remove.append(x)
+
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
-                 rem.append(pipe) 
-                 
+                rem.append(pipe)
+
             if not pipe.passed and pipe.x < bird.x:
                 pipe.passed = True
                 add_pipe = True
-                
+
             pipe.move()
-            
-        if add_pipe: 
-                score += 1
-                for ge in ge:
-                    g.fitness += 5
-                pipes.append(Pipe(600)) 
-                
+
+        for x in birds_to_remove:
+            birds.pop(x)
+            nets.pop(x)
+            ge.pop(x)
+
+        if add_pipe:
+            score += 1
+            for g in ge:
+                g.fitness += 5
+            pipes.append(Pipe(600))
+
         for r in rem:
             pipes.remove(r)
-            
-        for x, bird in enumerate(birds):            
-            if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
-                birds.pop(x)
-                nets.pop(x)      
-                ge.pop(x)  
-                
+
         if score > 50:
-            break      
+            break
 
         base.move()
-        draw_window(win, bird, pipes, base, score, GEN)
+        draw_window(win, birds, pipes, base, score, GEN)
 
 
 def run(config_path):
